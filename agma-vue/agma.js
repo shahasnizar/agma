3b18c38cea8fbca5830c433057ee38012b6ec9c2
Nihas Nizar <nihasnizar@gmail.com>
2021-02-15 21:45:24 +0530

    added .js

---
+++ agma-vue/agma.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Setting up the public directory
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
